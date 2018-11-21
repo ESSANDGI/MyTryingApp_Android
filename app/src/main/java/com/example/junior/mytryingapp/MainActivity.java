8b636e5ffc02fa17c17eccb0fd21ffbbc2acd940
ESSANDGI <juniornelson.sulcagonzales@gmail.com>
2018-11-21 22:36:04 +0100

    aggiunti componenti View alla activity_main.xml

--- app/src/main/java/com/example/junior/mytryingapp/MainActivity.java
+++ app/src/main/java/com/example/junior/mytryingapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState );
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
 //      la attività è creata
