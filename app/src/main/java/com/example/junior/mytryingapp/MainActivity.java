df6d725083d89d75749d1f0740c10c1b3a60a33a
ESSANDGI <juniornelson.sulcagonzales@gmail.com>
2018-11-21 23:40:29 +0100

    scritta parte di codice che mi permette di settare i componenti View al momento del onCreate

--- app/src/main/java/com/example/junior/mytryingapp/MainActivity.java
+++ app/src/main/java/com/example/junior/mytryingapp/MainActivity.java
@@ -2,19 +2,33 @@ package com.example.junior.mytryingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText et1;
+    private EditText et2;
+    private TextView tv1;
 //    questi mtodi overraidati non sono altro che i cicli di vita di un'aaplicazione
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState );
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
 //      la attività è creata
+
+//      La classe R è una classe che mi permette di collegare xml con java
+        et1 = (EditText)findViewById(R.id.txt_num1);
+        et2 = (EditText)findViewById(R.id.txt_num2);
+        tv1 = (TextView)findViewById(R.id.txt_result);
+    }
+
+//  creiamo un metodo per sommare
+    public void sum(View view){
+        
     }
 
     @Override
